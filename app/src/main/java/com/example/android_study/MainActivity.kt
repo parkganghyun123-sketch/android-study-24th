ac4e958487b70f19b729adfb2b85ea5d1a094132
GangHyun <parkganghyun123@gmail.com>
2026-04-06 20:01:26 +0900

    week2

--- app/src/main/java/com/example/android_study/MainActivity.kt
+++ app/src/main/java/com/example/android_study/MainActivity.kt
@@ -4,13 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.android_study.ui.theme.AndroidstudyTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +37,16 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidstudyTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "박강현",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+
+                        contentAlignment = Alignment.BottomCenter
+                    ) {
+                        // TODO
+                        ProfileCard("박강현")
+                    }
                 }
             }
         }
@@ -31,17 +54,53 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "202355537 $name!",
-        modifier = modifier
-    )
+fun ProfileCard(name: String) {
+    Row(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(1f)
+            .background(Color(0xFFF0F0F0), shape = RoundedCornerShape(12.dp))
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        // 1. 프로필 아이콘
+        Icon(
+            imageVector = Icons.Default.AccountCircle,
+            contentDescription = null,
+            modifier = Modifier.size(50.dp),
+            tint = Color.Gray
+        )
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        // 2. 이름과 설명
+        Column {
+            Text(
+                text = name,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black
+            )
+            Text(
+                text = "Pusan National University \nComputer Science & Engineering",
+                fontSize = 14.sp,
+                color = Color.DarkGray
+            )
+            Text(
+                text = "I wanna go to sleep",
+                fontSize = 14.sp,
+                color = Color.LightGray
+
+
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AndroidstudyTheme {
-        Greeting("박강현")
+        ProfileCard("박강현")
     }
 }
